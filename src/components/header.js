4fa3831291376f84f58ec10029712236643cadc6
Julio Cesar Agüero <julio.aguero.gomez@gmail.com>
2023-12-06 13:14:22 -0600

    header completo

--- src/components/header.js
+++ src/components/header.js
@@ -5,20 +5,36 @@ const avatarImage = "../assets/images/image-avatar.png";
 import styles from "../styles/header-styles";
 
 export class Header extends LitElement {
+  static get properties() {
+    return {
+      Menu: { type: String },
+    };
+  }
   static get styles() {
     return [styles];
+  }
+  constructor() {
+    super();
+    this.Menu = true;
+  }
+  firstUpdated() {
+    this._resize();
   }
   render() {
     return html`
       <header class="header">
-        <img class="header__menu-icon" src="${menuIcon}" alt="menu" />
+        <img @click="${this._handleOpenMenu}" class="header__menu-icon" src="${menuIcon}" alt="menu" />
         <img class="header__logo" src="${logo}" alt="" />
-        <nav class="header__nav">
-          <a href="#">Collections</a>
-          <a href="#">Men</a>
-          <a href="#">Women</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
+        <nav id="nav" class="${this.Menu ? 'header__nav header__nav--open' : 'header__nav'}">
+        <div class="container-nav__close-menu" >
+          <img src="../../assets/images/icon-close.svg" class="nav__close-menu" @click="${this._handleCloseMenu}">
+        </div>
+        
+          <a class="nav__link" href="#">Collections</a>
+          <a class="nav__link" href="#">Men</a>
+          <a class="nav__link" href="#">Women</a>
+          <a class="nav__link" href="#">About</a>
+          <a class="nav__link" href="#">Contact</a>
         </nav>
         <div class="header__cart">
           <button>
@@ -29,5 +45,19 @@ export class Header extends LitElement {
       </header>
     `;
   }
+  _handleOpenMenu() {
+    console.log("click");
+    this.Menu = true;
+  }
+  _handleCloseMenu() {
+    console.log("click");
+    this.Menu = false;
+  }
+  _resize() {
+    window.addEventListener("resize", () => {
+      this.Menu = false;
+    });
+  }
+
 }
 customElements.define("main-header", Header);
--- src/styles/header-styles.js
+++ src/styles/header-styles.js
@@ -4,49 +4,104 @@ export default css`
     width: 100%;
     height: 100px;
     display: flex;
-    gap: 0;
+    gap: 5em;
     align-items: center;
     justify-content: space-between;
     margin: auto;
   }
   .header__nav{
-    display: none;
+    height: 0;
+    opacity: 0;
     list-style: none;
+    
   }
-  .header__nav a{
+  .header__nav--open{
+    z-index: 100;
+    opacity: 1;
+    display: flex;
+    position: absolute;
+    background-color: #f2f2f2;
+    width: 300px;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    flex-direction: column;
+    justify-content:center;
+    align-items: center;
+    transition: all .3s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+  .header__nav .nav__close-menu{
+    position: absolute;
+    top: 5%;
+    right: 5%;
+    display: grid;
+    place-content: center;
+    background-color: transparent;
+    width: 30px;
+    height: 30px;
+    padding: 0em;
+    border: none;
+    cursor: pointer;
+    font-size: 2em;
+    font-weight: 800;
+    border-radius: 10px;
+    transition: all .3s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+  .header__nav .nav__close-menu:hover{
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+  }
+  .header__nav .nav__link{
     color: black;
     text-decoration: none;
+    font-weight: 900;
+    font-size: 1.2em;
+    letter-spacing: 0.1em;
+    padding: 1.2em 0.7em;
+    cursor: pointer;
   }
   .header__menu-icon{
+    margin: auto 1em;
     width: 45px;
     height: 25px;
     border-radius: 1px;
+    color: black;
   }
   .header__logo{
     position: absolute;
-    left: 10%;
-    width: 100px;
-    height: 30px;
+    left: 4.5em;
+    width: 150px;
+    height: 25px;
   }
   .header__cart{
+    height: 60%;
     display: flex;
-    gap: 0.5em;
+    gap: 1.5em;
   }
-  @media (min-width: 968px) {
+  .header__cart button{
+    border: none;
+    height: 100%;
+    background-color: transparent;
+  }
+  @media (min-width: 1068px) {
     .header{
-        width: 100%;
+      width: 80%;
       gap: 5em;
     }
     .header__menu-icon{
       display: none;
     }
     .header__nav{
+      opacity: 1;
       display: flex;
       gap: 2.5em;
       flex: 1;
       justify-content: flex-start;
       align-items: center;
-      
+    }
+    .header__nav a{
+      padding: 0;
+      font-weight: 300;
+      font-size: 1em;
     }
     .header__logo{
       position: relative;
